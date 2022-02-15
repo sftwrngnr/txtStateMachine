ce1af4c90a2053a380e07a1aa09ba7ad4dee9006
Dan Henderson <sftwrngnr66@gmail.com>
2022-02-14 23:44:09 -0700

    Updated vars, added testdata, updated example.

--- pkg/vars.go
+++ pkg/vars.go
@@ -1,4 +1,27 @@
 package txtfsm
 
+type FSMVar int
+
+const (
+	VInt FSMVar = iota
+	VFloat
+	VString
+)
+
 type Vars struct {
+	Name   string
+	VType  FSMVar
+	CurVal interface{}
+}
+
+func (v *Vars) AsInt() int {
+	return v.CurVal.(int)
+}
+
+func (v *Vars) AsFloat() float32 {
+	return v.CurVal.(float32)
+}
+
+func (v *Vars) AsString() string {
+	return v.CurVal.(string)
 }
